3b9c01a4b2f7afd25d15e0fb578fd8a8bef759dd
Marjun668 <marjunboctulan@gmail.com>
2023-11-25 20:43:48 +0800

    Added_book_of_the_week

---
+++ Java/javascript.js
@@ -0,0 +1,18 @@
+  function closeZoom() {
+    var zoomContainer = document.getElementById('BOTWcont');
+    zoomContainer.style.display = 'none';
+  }
+
+  function book11() {
+    var zoomContainer = document.getElementById('BOTWcont');
+    var zoomedImg = document.getElementById('zoomedImg');
+    zoomedImg.src = './Image/Book Info/Name Above All Name Info.png';
+    zoomContainer.style.display = 'flex';
+  }
+
+  function book5() {
+    var zoomContainer = document.getElementById('BOTWcont');
+    var zoomedImg = document.getElementById('zoomedImg');
+    zoomedImg.src = './Image/Book Info/Dig Deeper info.png';
+    zoomContainer.style.display = 'flex';
+  }
